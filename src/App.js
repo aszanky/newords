19782833fb4ec5cfb89015676fb2a536b4ee9d44
“Asaduddin” <asaduddin.zanky@telkom.co.id>
2024-10-07 14:52:49 +0700

    initial commit newords fe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from "react";
+import { Table, Button, Typography, Layout, Flex, message } from 'antd'; 
+// import { WordForm } from './components/wordForm.jsx';
+import WordForm from './components/wordForm/wordForm'
 import './App.css';
 
+const { Text, Title } = Typography;
+const { Header, Footer, Content } = Layout;
+
 function App() {
+  const baseURL = "http://localhost:8090/words";
+
+  const [showTable, setShowTable] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [word, setWord] = useState('');
+  const [indonesian, setIndonesian] = useState('');
+  const [notes, setNotes] = useState('');
+
+  const getListWords = async () => {
+    setShowTable(!showTable);
+
+    if (!showTable) {  // Only fetch when showing the table
+      setLoading(true);
+
+      try {
+        const res = await fetch(`${baseURL}/get`);  // Example API
+        const result = await res.json();
+
+        const formattedData = result.response.map(item => ({
+          id: item.id,
+          word: item.word,
+          indonesian: item.indonesian,
+          notes: item.notes,
+        }));
+
+        setData(formattedData);
+      } catch (err) {
+        setData(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
+  const addYourWords = async () => {
+    setShowForm(!showForm);
+
+    const myWords = { word, indonesian, notes };
+
+    try {
+      const response = await fetch(`$baseURL/add`, {  // Replace with your API URL
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(myWords),  // Convert JavaScript object to JSON string
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+      message.success(`word ${result.word} added successfully!`);
+    } catch (error) {
+      console.error("Error posting data:", error);
+      message.error('Failed to add word. Please try again.');
+    }
+  };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Word',
+      dataIndex: 'word',
+      key: 'word',
+    },
+    {
+      title: 'Indonesia',
+      dataIndex: 'indonesian',
+      key: 'indonesian',
+    },
+    {
+      title: 'Notes',
+      dataIndex: 'notes',
+      key: 'notes',
+    },
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flex gap="middle" wrap>
+        <Layout>
+          <Header className="app-header">Newords</Header>
+          <Text style={{textAlign: 'center', marginTop:'40px', fontWeight: 'bold', fontSize: '20px'}}>
+            Remember your new words here!
+          </Text>
+          <Content className="content-style">
+            <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: '30px'}}>
+              Add Your Words
+            </Text>
+
+            <WordForm />
+
+            <Button type="primary" onClick={getListWords}>
+              {showTable ? 'Hide Words' : 'Your Words Collection'}
+            </Button>
+
+            {/* {loading && <Spin size="large" />} */}
+
+            {showTable && !loading && (
+              <Table columns={columns} dataSource={data} pagination={false} style={{marginBottom: '30px'}} />
+            )}
+          </Content>
+          <Footer className="footer-style">by Newords!</Footer>
+        </Layout>
+      </Flex>
     </div>
   );
 }
---
+++ src/components/wordForm/wordForm.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Form, Input, Button, message } from 'antd';
+import './wordForm.css'
+
+
+export default class WordForm extends Component {
+  onFinish = async (values) => {
+    const baseURL = "http://localhost:8090/words";
+
+    try {
+      // Replace with your API URL
+      const response = await fetch(`${baseURL}/add`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values), // Send the form values as JSON
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const result = await response.json();
+      message.success(`word ${result.word} added successfully!`);
+    } catch (error) {
+      console.error("Error adding word:", error);
+      message.error('Failed to add user. Please try again.');
+    }
+  };
+
+  render() {
+    return (
+      <Form
+        name="wordForm"
+        layout="vertical"
+        onFinish={this.onFinish}
+        className="small-form"
+      >
+        <Form.Item
+          label="Word"
+          name="word"
+          rules={[{ required: true, message: 'Please input your new word!' }]} // Validation rule
+        >
+          <Input placeholder="Enter your Word" />
+        </Form.Item>
+
+        <Form.Item
+          label="Indonesia"
+          name="indonesian"
+          rules={[
+            { required: true, message: 'Please input indonesia translation!' }
+            // { type: 'email', message: 'The input is not valid E-mail!' }, // Email validation
+          ]}
+        >
+          <Input placeholder="Enter Indonesia translation" />
+        </Form.Item>
+
+        <Form.Item
+          label="Notes"
+          name="notes"
+          rules={[{ required: true, message: 'Please input Notes!' }]} // Validation rule
+        >
+          <Input placeholder="Enter your Notes" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+};
